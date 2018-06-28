c12919f5f5e017da076900c1f22b59188df2dc6e
MaxOniHub <onyshchenko.max@gmail.com>
2018-06-28 21:30:25 +0300

    - fixes

--- frontend/components/views/company-clients-widget/view.php
+++ frontend/components/views/company-clients-widget/view.php
@@ -1,7 +1,7 @@
 <?php if (!empty($items)):?>
 <ul class="company-clients align-left md-align-center">
 <?php foreach ($items as $item):?>
-    <li class="violet"><a href="<?= $item ?>"><?= $item ?></a></li>
+    <li class="violet"><span><?= $item ?></span></li>
 <?php endforeach;?>
 </ul>
 <?php endif; ?>
