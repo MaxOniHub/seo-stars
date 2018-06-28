a6191237506335afc60a1d07bf9b3471c5c21fe7
MaxOniHub <onyshchenko.max@gmail.com>
2018-06-28 21:43:02 +0300

    - seo tags

--- frontend/views/activity-directions/index.php
+++ frontend/views/activity-directions/index.php
@@ -1,3 +1,16 @@
+<?php
+$this->title = $activity->seo_title;
+$this->registerMetaTag([
+    'name' => 'description',
+    'content' => $activity->seo_desc
+]);
+$this->registerMetaTag([
+    'name' => 'keywords',
+    'content' => $activity->seo_keys
+]);
+
+?>
+
 <div class="align-center">
     <div class="section-title blue-lined bottom-offset"><?= $activity->getTitle(); ?></div>
 </div>
