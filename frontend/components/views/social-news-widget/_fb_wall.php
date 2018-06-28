91e7af49bdc28d4332b35b29bb65f3f3f177a656
MaxOniHub <onyshchenko.max@gmail.com>
2018-06-29 01:07:41 +0300

    - fixed news wall

--- frontend/components/views/social-news-widget/_fb_wall.php
+++ frontend/components/views/social-news-widget/_fb_wall.php
@@ -1,24 +1,26 @@
-<hr/>
-<div class="row">
-    <h2>Новости от <?= $entity["name"]; ?></h2>
+<div class="container wall">
     <div class="row">
-        <?php
-        for ($i = 0; $i < count($wall); $i++) {
-            if ($wall[$i]->message) { ?>
-                <div class="col-sm-6 col-xs-12">
-                    <div class="panel panel-info">
-                        <div class="panel-body">
-                            <div class="wall_text">
-                                <?= $widget->parseStringToLink($wall[$i]->message); ?>
+        <h2>Новости от <?= $entity["name"]; ?></h2>
+        <div class="row">
+            <?php
+            for ($i = 0; $i < count($wall); $i++) {
+                if ($wall[$i]->message) { ?>
+                    <div class="col-sm-12 col-xs-12 news-record">
+                        <div class="panel panel-info">
+                            <div class="panel-body">
+                                <div class="wall_text">
+                                    <?= $widget->parseStringToLink($wall[$i]->message); ?>
+                                </div>
+                            </div>
+                            <div class="panel-footer">
+                                <?= $widget->getDateByFormat(strtotime($wall[$i]->created_time)); ?>
                             </div>
                         </div>
-                        <div class="panel-footer">
-                            <?= $widget->getDateByFormat(strtotime($wall[$i]->created_time)); ?>
-                        </div>
                     </div>
-                </div>
-            <?php }
-        } ?>
+                    <hr/>
+                <?php }
+            } ?>
+        </div>
     </div>
 </div>
 
--- frontend/components/views/social-news-widget/_vk_wall.php
+++ frontend/components/views/social-news-widget/_vk_wall.php
@@ -1,40 +1,38 @@
-<?php if ($wall) { ?>
-    <div class="container wall">
-        <hr/>
-        <div class="row">
-            <h2>Новости от <?= $entity["name"]; ?></h2>
-            <?php
-            for ($i = 1; $i < count($wall); $i++) {
-                ?>
-                <div class="col-sm-6 col-xs-12">
-                    <div class="panel panel-info">
-                        <div class="panel-body">
-                            <div class="wall_text">
-                                <?= $widget->parseStringToLink($wall[$i]->text); ?>
-                            </div>
-                            <div class="row photos">
-                                <?php
-                                $imgs = $wall[$i]->attachments;
-                                $arr = [];
-                                for ($j = 0; $j < count($imgs); $j++) {
-                                    if ($imgs[$j]->photo->src) $arr[] = '<img src="' . $imgs[$j]->photo->src_big . '" alt="' . $i . $j . '"/>';
-                                }
-                                $col = count($arr);
-                                if ($col >= 4) $col = 3; else if ($col == 1) $col = 12; else if ($col == 3) $col = 4; else if ($col == 2) $col = 6;
-                                for ($j = 0; $j < count($arr); $j++) {
-                                    ?>
-                                    <div class="col-md-<?= $col; ?>">
-                                        <?= $arr[$j]; ?>
-                                    </div>
-                                <?php } ?>
-                            </div>
+<div class="container wall">
+    <div class="row">
+        <h2>Новости от <?= $entity["name"]; ?></h2>
+        <?php
+        for ($i = 1; $i < count($wall); $i++) {
+            ?>
+            <div class="col-sm-12 col-xs-12 news-record">
+                <div class="panel panel-info">
+                    <div class="panel-body">
+                        <div class="wall_text">
+                            <?= $widget->parseStringToLink($wall[$i]->text); ?>
                         </div>
-                        <div class="panel-footer">
-                            <?= $widget->getDateByFormat($wall[$i]->date) ?>
+                        <div class="row photos">
+                            <?php
+                            $imgs = $wall[$i]->attachments;
+                            $arr = [];
+                            for ($j = 0; $j < count($imgs); $j++) {
+                                if ($imgs[$j]->photo->src) $arr[] = '<img src="' . $imgs[$j]->photo->src_big . '" alt="' . $i . $j . '"/>';
+                            }
+                            $col = count($arr);
+                            if ($col >= 4) $col = 3; else if ($col == 1) $col = 12; else if ($col == 3) $col = 4; else if ($col == 2) $col = 6;
+                            for ($j = 0; $j < count($arr); $j++) {
+                                ?>
+                                <div class="col-md-<?= $col; ?>">
+                                    <?= $arr[$j]; ?>
+                                </div>
+                            <?php } ?>
                         </div>
                     </div>
+                    <div class="panel-footer">
+                        <?= $widget->getDateByFormat($wall[$i]->date) ?>
+                    </div>
                 </div>
-            <?php } ?>
-        </div>
+            </div>
+            <hr/>
+        <?php } ?>
     </div>
-<?php } ?>
+</div>
